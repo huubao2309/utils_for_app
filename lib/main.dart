b11c73d67fbb7efa5e7be1b3c21b84ca79d44bc2
Huu Bao Nguyen <huubao2309@gmail.com>
2020-03-23 00:21:42 +0700

    commit for multi language

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,53 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-void main() => runApp(MyApp());
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() => runApp(
+      EasyLocalization(
+        supportedLocales: [
+          Locale('vi', 'VN'),
+          Locale('en', 'US'),
+        ],
+        path: 'resources/langs',
+        child: MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.light(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Utils for app'),
-        ),
-        body: Center(
-          child: Text('Test app'),
-        ),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        EasyLocalization.of(context).delegate,
+      ],
+      supportedLocales: EasyLocalization.of(context).supportedLocales,
+      locale: EasyLocalization.of(context).locale,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("title").tr(context: context),
+      ),
+      body: Center(
+        child: Text('Abc'),
       ),
     );
   }
